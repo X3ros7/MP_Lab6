011a0c6a67dcc21aeb4ab3e6dd7b0fd938a35883
X3ros7 <a.a.prosv2005@gmail.com>
2024-11-10 10:20:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mp_lab6/KtorClient.kt
@@ -0,0 +1,20 @@
+package com.example.mp_lab6
+
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.util.logging.Logger
+import kotlinx.serialization.json.Json
+
+val client = HttpClient {
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+        })
+    }
+    install(Logging) {
+        level = LogLevel.BODY
+    }
+}
